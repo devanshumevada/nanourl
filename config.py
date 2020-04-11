84d5943370d8160806871128ba70172af4ab0b6b
Devanshu Mevada <devanshumevada@Devanshus-MacBook-Air.local>
2020-04-11 20:08:06 +0530

    removed confif.py from gitignore for deployment

---
+++ config.py
@@ -0,0 +1,3 @@
+MONGO_DB_CONNECTION='mongodb+srv://admin:admin@cluster0-jfdmf.mongodb.net/test?retryWrites=true&w=majority'
+SECRET_KEY="\x9a#\xc8'\xa1T\xdc{A#{N\x90\x94\xf2\xf3"
+SEND_GRID_API_KEY='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'

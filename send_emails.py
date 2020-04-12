810cf65a907ae8f765c1e769ca244f54a151f16d
Devanshu Mevada <devanshumevada@Devanshus-MacBook-Air.local>
2020-04-12 12:02:59 +0530

    Added forgot_password functionality, made the /login url with parameters to redirect to /login so that uneccesary pop-ups are avoided

--- send_emails.py
+++ send_emails.py
@@ -1,10 +1,15 @@
-from config import SEND_GRID_API_KEY
+from config import SEND_GRID_API_KEY, SEND_GRID_API_LINK, SEND_GRID_API_HEADERS
 import requests
 import json
 
 def send_account_verification_email(user_email,user_id):
-    data = json.dumps({"personalizations": [{"to": [{"email": user_email}]}],"from": {"email": "verify_nanourl@nanourl.xyz"},"subject": "Please verify your user account","content": [{"type": "text/plain", "value": "Please verify your email by going to http://nanourl1.herokuapp.com/"+user_id+"/verify"}]})
-    print(data)
-    requests.post(url='https://api.sendgrid.com/v3/mail/send', data=data, headers={'Content-Type':'application/json', 'Authorization':'Bearer '+SEND_GRID_API_KEY})
+    data = json.dumps({"personalizations": [{"to": [{"email": user_email}]}],"from": {"email": "verify_nanourl@nanourl.xyz"},"subject": "Please verify your user account","content": [{"type": "text/plain", "value": "Please verify your email by going to http://www.nanourl.xyz/"+user_id+"/verify"}]})
+    requests.post(url=SEND_GRID_API_LINK, data=data, headers=SEND_GRID_API_HEADERS)
+
+def send_forgot_password_instructions_email(user_email, user_password):
+        data = json.dumps({"personalizations": [{"to": [{"email": user_email}]}],"from": {"email": "forgot_password@nanourl.xyz"},"subject": "Your user account password","content": [{"type": "text/plain", "value": "Your user account passowrd is: "+user_password}]})
+        requests.post(url=SEND_GRID_API_LINK, data=data, headers=SEND_GRID_API_HEADERS)
+
+    
 
     
--- user_auth_routes.py
+++ user_auth_routes.py
@@ -2,7 +2,7 @@ from app import app
 from flask import request, redirect, url_for, session, render_template
 from db import db_links, db_user
 from helper import user_logged_in
-from send_emails import send_account_verification_email
+from send_emails import send_account_verification_email, send_forgot_password_instructions_email
 from bson import ObjectId
 
 
@@ -26,13 +26,20 @@ def login():
         if request.method=='POST':
                user=db_user.find_one({'email':request.form['email'],'password':request.form['password']})
                if user is None:
-                       return render_template('login.html',user_not_exists=True)
+                       return redirect(url_for('login',user_not_exists=True))
                if user['is_verified']==0:
-                       return render_template('login.html', account_verification=False)
+                       return redirect(url_for('login', account_verification=False))
                else:
                         session['user']=str(user['_id'])
                         return redirect('/')
-        return render_template('login.html', user_already_exists=request.args.get('user_already_exists'),email_sent=request.args.get('email_sent'))
+
+        return render_template('login.html', 
+                user_already_exists=request.args.get('user_already_exists'),
+                email_sent=request.args.get('email_sent'),
+                user_not_exists=request.args.get('user_not_exists'),
+                account_verification=request.args.get('account_verification'),
+                forgot_password_instructions=request.args.get('forgot_password_instructions')
+        )
 
 @app.route('/logout')
 def logout():
@@ -45,3 +52,12 @@ def verify_user_account(user_id):
                 return '<h4>Account already verified, please <a href="/login">Log In</a></h4>'
         db_user.update_one({'_id':ObjectId(user_id)},{'$set':{'is_verified':1}})
         return '<h4>Account Verified! <a href="/login">Click Here</a> to log in'
+
+@app.route('/forgot_password', methods=['POST'])
+def forgot_password():
+        email = request.form['recipient-email']
+        user = db_user.find_one({'email':email})
+        if user is None:
+                return redirect(url_for('login',user_not_exists=True))
+        send_forgot_password_instructions_email(user['email'],user['password'])
+        return redirect(url_for('login', forgot_password_instructions=True))

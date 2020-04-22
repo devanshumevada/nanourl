634c6bd3265b0d92c2dc1e61ec3b2f1f4e5561fb
Devanshu Mevada <devanshumevada@Devanshus-MacBook-Air.local>
2020-04-22 21:48:06 +0530

    fixed redirect when user not logged in and /dashboard route is accessed

--- general_routes.py
+++ general_routes.py
@@ -2,11 +2,11 @@ from app import app
 from flask import render_template, url_for, request, session, redirect
 from db import db_user, db_links
 from bson import ObjectId
-from helper import get_current_user, get_current_user_links
+from helper import get_current_user, get_current_user_links, user_logged_in
 
 @app.route('/')
 def index():
-        if 'user' not in session:
+        if not user_logged_in():
                 return redirect(url_for('login'))
                 #return render_template('login.html', email_not_sent=request.args.get('email_not_sent'), email_sent=request.args.get('email_sent'),account_verification=request.args.get('account_verification'))
         else:
@@ -15,7 +15,9 @@ def index():
 
 @app.route('/dashboard')
 def dashboard():
-        return render_template('dashboard.html',links=get_current_user_links(),user=get_current_user(), url_not_valid=request.args.get('url_not_valid'))
+        if user_logged_in():
+                return render_template('dashboard.html',links=get_current_user_links(),user=get_current_user(), url_not_valid=request.args.get('url_not_valid'))
+        return redirect(url_for('login'))
 
 
 

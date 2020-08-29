b0d6777955b28b85dba5f77e30b097b2a9c22dad
Devanshu Mevada <devanshumevada@Devanshus-MacBook-Air.local>
2020-08-29 19:19:10 +0530

    environment variables configured

--- config.py
+++ config.py
@@ -1,3 +1,14 @@
+import os
+
+MONGO_DB_CONNECTION = os.environ['MONGO_DB_CONNECTION']
+SECRET_KEY = os.environ['SECRET_KEY']
+SEND_GRID_API_LINK = os.environ['SEND_GRID_API_LINK']
+SEND_GRID_API_KEY  = os.environ['SEND_GRID_API_KEY']
+SEND_GRID_API_HEADERS={'Content-Type':'application/json', 'Authorization':'Bearer '+SEND_GRID_API_KEY}
+MEMC_SERVERS = os.environ['MEMC_SERVERS']
+MEMC_USERNAME = os.environ['MEMC_USERNAME']
+MEMC_PASSWORD = os.environ['MEMC_PASSWORD']
+'''
 MONGO_DB_CONNECTION='mongodb+srv://admin:admin@cluster0-jfdmf.mongodb.net/test?retryWrites=true&w=majority'
 SECRET_KEY="\x9a#\xc8'\xa1T\xdc{A#{N\x90\x94\xf2\xf3"
 SEND_GRID_API_KEY='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'
@@ -5,4 +16,5 @@ SEND_GRID_API_LINK='https://api.sendgrid.com/v3/mail/send'
 SEND_GRID_API_HEADERS={'Content-Type':'application/json', 'Authorization':'Bearer '+SEND_GRID_API_KEY}
 MEMC_SERVERS='mc2.dev.ec2.memcachier.com:11211'
 MEMC_USERNAME='6EFBA2'
-MEMC_PASSWORD='F1F922A5717220DF21CDE3925AD668CA'
+MEMC_PASSWORD='F1F922A5717220DF21CDE3925AD668CA
+'''
